4dd40a1065b9636f8dd3870b3f1d2600ba11bdbc
Carlos Xavier <carlos.lipe559@hotmail.com>
2023-01-09 17:44:22 -0300

    gif/float comments/comments

--- js/script.js
+++ js/script.js
@@ -2,31 +2,31 @@ function faq() {
 
     const pergunta = document.querySelectorAll(".faq");
     const resposta = document.querySelectorAll(".faq_text");
-      
-        for (let i = 0; i < pergunta.length; i++) {
 
-            pergunta[i].addEventListener("click", () => {
-               
-                    
-                    if ( pergunta[i].classList.contains('fechar') ) {
-                        pergunta[i].classList.toggle('fechar');
-                        resposta[i].classList.toggle('ativar');
-                    } else {
-                        pergunta[i].classList.add('fechar');
-                        resposta[i].classList.add('ativar');
-                    }
+    for (let i = 0; i < pergunta.length; i++) {
 
-            });
-            
-        }
+        pergunta[i].addEventListener("click", () => {
+
+
+            if (pergunta[i].classList.contains('fechar')) {
+                pergunta[i].classList.toggle('fechar');
+                resposta[i].classList.toggle('ativar');
+            } else {
+                pergunta[i].classList.add('fechar');
+                resposta[i].classList.add('ativar');
+            }
+
+        });
+
+    }
 
 }
 
 faq();
 
 function img_mobile() {
-    let largura = window.innerWidth;
-    if( largura <= 425 ) {
+    var largura = window.innerWidth;
+    if (largura <= 425) {
         document.querySelector(".slide_desktop_1").src = 'img/banner_1_mobile.png';
         document.querySelector(".slide_desktop_2").src = 'img/banner_2_mobile.png';
     }
@@ -36,34 +36,147 @@ function img_mobile() {
 window.addEventListener('load', img_mobile());
 
 
-function menu(){
+function menu() {
     $(".menu_opener").click(
-        function(){
-            
-            $('.area_menu_mobile_total').css( 'display', '-webkit-flex' ).css( 'top', '120px' ).css( 'animation', 'move 1s ease' );
-            setTimeout( ()=> {
-                $('.menu_opener').css( 'display', 'none', 'important' );
-                $('.menu_close').css( 'display', '-webkit-flex' );
-            }, 600 );
-        }  
+        function () {
+
+            $('.area_menu_mobile_total').css('display', '-webkit-flex').css('top', '120px').css('animation', 'move 1s ease');
+            setTimeout(() => {
+                $('.menu_opener').css('display', 'none', 'important');
+                $('.menu_close').css('display', '-webkit-flex');
+            }, 600);
+        }
     );
 
     $(".menu_close").click(
-        function() {
-            $('.area_menu_mobile_total').css( 'top', '-100%' ).css('animation', 'move 1s ease' );
-            setTimeout( () => {
-                if($('.area_menu_mobile_total').css('top', '-100%')) {
+        function () {
+            $('.area_menu_mobile_total').css('top', '-100%').css('animation', 'move 1s ease');
+            setTimeout(() => {
+                if ($('.area_menu_mobile_total').css('top', '-100%')) {
                     $('.area_menu_mobile_total').css('-webkit-flex', 'none', 'important');
                 }
             }, 600);
 
-            setTimeout( ()=> {
-                $('.menu_opener').css( 'display', '-webkit-flex' );
-                $('.menu_close').css( 'display', 'none', 'important' );
-            }, 100 );
-            
+            setTimeout(() => {
+                $('.menu_opener').css('display', '-webkit-flex');
+                $('.menu_close').css('display', 'none', 'important');
+            }, 100);
+
         }
-        
+
     );
 }
 menu();
+
+function gif() {
+    $('.img_2_bg img').css('animation', '');
+
+    setInterval(() => {
+        $('.img_2_bg img').css('animation', 'gif 400ms ease-in');
+        document.querySelector('.img_2_bg img').src = 'img/armadilhaCheia.gif';
+        $('.gif').css('max-width', '400px');
+    }, 8000);
+
+    setInterval(() => {
+        $('.img_2_bg img').css('animation', 'gif_2 400ms ease');
+        document.querySelector('.img_2_bg img').src = 'img/img_2.png';
+        $('.gif').css('max-width', '474px');
+    }, 16000);
+
+    $('.img_2_bg_mobile img').css('animation', '');
+
+    setInterval(() => {
+        $('.img_2_bg_mobile img').css('animation', 'gif 400ms ease-in');
+        document.querySelector('.img_2_bg_mobile img').src = 'img/armadilhaCheia.gif';
+        $('.gif_mobile').css('max-width', '360px');
+
+        var largura = window.innerWidth;
+        if (largura <= 425) {
+            $('.gif_mobile').css('max-width', '300px');
+        }
+
+    }, 8000);
+
+    setInterval(() => {
+        $('.img_2_bg_mobile img').css('animation', 'gif_2 400ms ease');
+        document.querySelector('.img_2_bg_mobile img').src = 'img/img_2.png';
+        var largura = window.innerWidth;
+        if (largura <= 425) {
+            $('.gif_mobile').css('max-width', '320px');
+        }
+    }, 16000);
+
+
+}
+gif();
+
+var min_u = 10;
+var max_u = 30;
+
+
+
+function getRandomArbitrary() {
+    let number_unidades_1 = Math.random() * (max_u - min_u) + min_u;
+    let number_unidades_2 = Math.random() * (max_u - min_u) + min_u;
+    let number_kits_4 = 4;
+    let number_kits_6 = 6;
+
+    //Primeiro float comment
+    $('.area_float_buy').css('animation', '');
+    setTimeout(() => {
+        $('.area_float_buy').css('display', 'flex').css('animation', 'comment 400ms ease-in');
+        document.querySelector('.float_buy h2').innerHTML = "João comprou um kit de " + number_kits_4 + " unidades. Restam " + parseInt(number_unidades_1) + " unidades.";
+    }, 120000);
+
+    setTimeout(() => {
+        $('.area_float_buy').css('display', 'none');
+    }, 125000);
+    //Segundo float comment
+    $('.area_float_buy').css('animation', '');
+    setTimeout(() => {
+        $('.area_float_buy').css('display', 'flex').css('animation', 'comment 400ms ease-in');
+        document.querySelector('.float_buy h2').innerHTML = "Fernanda comprou um kit de " + number_kits_6 + " unidades. Restam " + parseInt(number_unidades_2) + " unidades.";
+    }, 240000);
+
+    setTimeout(() => {
+        $('.area_float_buy').css('display', 'none');
+    }, 245000);
+
+    //Terceiro float comment
+    $('.area_float_buy').css('animation', '');
+    setTimeout(() => {
+        $('.area_float_buy').css('display', 'flex').css('animation', 'comment 400ms ease-in');
+        document.querySelector('.float_buy h2').innerHTML = "Roberto comprou um kit de " + number_kits_6 + " unidades. Restam " + parseInt(number_unidades_1) + " unidades.";
+    }, 360000);
+
+    setTimeout(() => {
+        $('.area_float_buy').css('display', 'none');
+    }, 365000);
+
+    //Quarto float comment
+    $('.area_float_buy').css('animation', '');
+    setTimeout(() => {
+        $('.area_float_buy').css('display', 'flex').css('animation', 'comment 400ms ease-in');
+        document.querySelector('.float_buy h2').innerHTML = "Jorge comprou um kit de " + number_kits_4 + " unidades. Restam " + parseInt(number_unidades_2) + " unidades.";
+    }, 480000);
+
+    setTimeout(() => {
+        $('.area_float_buy').css('display', 'none');
+    }, 485000);
+
+    //Quinto float comment
+    $('.area_float_buy').css('animation', '');
+    setTimeout(() => {
+        $('.area_float_buy').css('display', 'flex').css('animation', 'comment 400ms ease-in');
+        document.querySelector('.float_buy h2').innerHTML = "Patrícia comprou um kit de " + number_kits_4 + " unidades. Restam " + parseInt(number_unidades_1) + " unidades.";
+    }, 600000);
+
+    setTimeout(() => {
+        $('.area_float_buy').css('display', 'none');
+    }, 650000);
+
+
+    //document.querySelector('.float_buy h2').innerHTML = "João comprou um kit de "+ number_kits_6 +" unidades. Restam " + parseInt(number_unidades) + " unidades."; 
+}
+
+getRandomArbitrary();
